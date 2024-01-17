6e9c9e28283c8ac38a5af0db3fa7e360e20813cd
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-01-17 13:59:55 +0300

    Версия 0.1

--- CircleRadiusCalc.cpp
+++ CircleRadiusCalc.cpp
@@ -1,8 +1,44 @@
 ﻿
 #include <iostream>
+using namespace  std;
+
+/// <summary>
+/// Метод для вычисления радиуса по длине окружности
+/// </summary>
+/// <param name="C"></param>
+/// <returns>Радиус</returns>
+double RadiusCalc(double C)
+{ 
+    double R = C / (2 * 3.14);
+    return R;
+}
+
+/// <summary>
+/// Метод для вычисления площади круга по радиусу
+/// </summary>
+/// <param name="R"></param>
+/// <returns>Площадь</returns>
+double AareaCalc(double R)
+{
+    double S = 3.14 * (R * R);
+    return S;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "Russian");
+
+    // Длина окружности полученная от пользователя
+    double curceOfCurcle;
+
+    cout << "Чтобы рассчитать площадь круга введите длину окражности : ";
+    cin >> curceOfCurcle;
+
+    double radius = RadiusCalc(curceOfCurcle);
+    double area = AareaCalc(radius);
+
+    cout << "Исходя из указанной вами длины окружности, площадь круга составляет : " << area << endl;
+    cout << "-------------------" << endl;
+    cout << "-------------------" << endl;
 }
 
